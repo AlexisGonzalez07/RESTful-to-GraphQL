953981de5e822839776e9c9566742257331c3d2b
Alexis Gonzalez <alexis.gonzalez07@gmail.com>
2021-12-10 09:23:20 -0800

    Changed server

--- server/server.js
+++ server/server.js
@@ -11,7 +11,9 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Serve up static assets
-app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
