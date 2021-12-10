3a0b2bd2e1ab6ad1694d63ccf7f897b23fdb1289
Alexis Gonzalez <alexis.gonzalez07@gmail.com>
2021-12-10 09:19:07 -0800

    Heroku deploy

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
 import { useQuery, useMutation } from '@apollo/client';
-import { QUERY_ME, QUERY_USER } from '../utils/queries';
+import { QUERY_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
 import { removeBookId } from '../utils/localStorage';
 import Auth from '../utils/auth';
